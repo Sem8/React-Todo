85b141ef89470a1b61a96f17b709ba587e03de2f
Sem8 <mssemmi8@gmail.com>
2019-02-13 18:01:30 -0500

    work in progress MVP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React from 'react';
 
+import TodoList from './components/TodoComponents/TodoList';
+
+const semTodo = [
+  {
+    task: 'Understand React',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Understand props and inheritance in React',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Not feel lost with React',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Not hate React',
+    id: Date.now(),
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      personTodo: semTodo,
+      task: '',
+      id: '',
+      completed: ''
+    };
+  }
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <div className='todoList'>
+          <h3>Tortured by React with the ugly Todo list MVP</h3>
+          <TodoList personTodo={this.state.personTodo}/>
+
+        </div>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+import Todo from './Todo';
+
+function TodoList(props) {
+    return (
+        <div className='confusingTodoList'>
+            {/* <h1>{props.aTodo.task}</h1> */}
+            {props.personTodo.map(eachTodo => (
+                <Todo secondConfusingTodoFile={eachTodo} />
+            ))}
+
+        </div>
+    );
+};
+
+export default TodoList;
